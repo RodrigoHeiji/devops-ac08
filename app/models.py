6edff0dd0c8f55649680f251f1a64325f7e9f70c
Tiago Feliciano <42190570+tiagoff99@users.noreply.github.com>
2018-10-26 22:40:49 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ from django.db import models
 # Create your models here.
 class Vestibular(models.Model):
     nome = models.CharField(max_length=200)
-class Candidatos(models.Model):
+class Candidato(models.Model):
     nome = models.CharField(max_length=200)
     instituicao = models.CharField(max_length=200)    
 class Curso(models.Model):
