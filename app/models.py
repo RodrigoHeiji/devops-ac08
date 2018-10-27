aed0a92844c4f3709d55daf1c51e2a01e6754b82
Bruno1405 <42422720+Bruno1405@users.noreply.github.com>
2018-10-26 21:15:58 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ Definition of models.
 from django.db import models
 
 # Create your models here.
+class Vestibular(models.Model):
+    nome = models.CharField(max_length=200)
 class Curso(models.Model):
     nome = models.CharField(max_length=200)
     periodo = models.CharField(max_length=50)
